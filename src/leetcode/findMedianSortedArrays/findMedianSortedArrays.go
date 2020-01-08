c537b887131628a7d0405374ff6ab1038d6eb3da
Lu <vongolaluan@hotmail.com>
2020-01-08 17:33:05 +0800

    findMedianSortedArrays

--- src/leetcode/findMedianSortedArrays/findMedianSortedArrays.go
+++ src/leetcode/findMedianSortedArrays/findMedianSortedArrays.go
@@ -3,11 +3,64 @@ package main
 import "fmt"
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	return 0.0
+	m, n := len(nums1), len(nums2)
+	if m > n {
+		nums1, nums2 = nums2, nums1
+		m, n = n, m
+	}
+	imin, imax, halfLen := 0, m, (m+n+1)/2
+	for imin <= imax {
+		i := (imin + imax) / 2
+		j := halfLen - i
+		if i < m && nums2[j-1] > nums1[i] {
+			imin = i + 1
+		} else if i > 0 && nums1[i-1] > nums2[j] {
+			imax = i - 1
+		} else {
+			var maxLeft int
+			if i == 0 {
+				maxLeft = nums2[j-1]
+			} else if j == 0 {
+				maxLeft = nums1[i-1]
+			} else {
+				maxLeft = max(nums1[i-1], nums2[j-1])
+			}
+
+			if (m+n)%2 == 1 {
+				return float64(maxLeft)
+			}
+
+			var minRight int
+			if i == m {
+				minRight = nums2[j]
+			} else if j == n {
+				minRight = nums1[i]
+			} else {
+				minRight = min(nums1[i], nums2[j])
+			}
+
+			return float64(maxLeft+minRight) / 2
+		}
+	}
+	return 0
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 func main() {
-	var nums1 []int = []int{1, 2}
-	nums2 := []int{3, 4}
+	var nums1 []int = []int{2, 3}
+	nums2 := []int{1}
 	fmt.Printf("%f", findMedianSortedArrays(nums1, nums2))
 }
