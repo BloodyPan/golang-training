a11bf612a420c5d05f8e55ef4fc0fba76cfeeb42
Lu <vongolaluan@hotmail.com>
2020-10-22 18:59:13 +0800

    Update test.go

--- src/test/test.go
+++ src/test/test.go
@@ -33,15 +33,6 @@ func main() {
 	/* 打印原始切片 */
 	fmt.Println("numbers ==", numbers)
 
-	/* 打印子切片从索引1(包含) 到索引4(不包含)*/
-	fmt.Println("numbers[1:4] ==", numbers[1:4])
-
-	/* 默认下限为 0*/
-	fmt.Println("numbers[:3] ==", numbers[:3])
-
-	/* 默认上限为 len(s)*/
-	fmt.Println("numbers[4:] ==", numbers[4:])
-
 	numbers1 := make([]int, 0, 5)
 	printSlice(numbers1)
 
@@ -53,22 +44,9 @@ func main() {
 	number3 := numbers[2:5]
 	printSlice(number3)
 
-	number4 := numbers[2:]
-	printSlice(number4)
-
-	number5 := numbers[:2]
-	printSlice(number5)
-
 	fmt.Println("0")
 	defer fmt.Println("defer")
 	fmt.Println("normal")
-
-	test := []int{1, 2, 3, 4, 5}
-	test1 := test[:2]
-	test2 := test1[2:5]
-	printSlice(test1)
-	printSlice(test2)
-	printSlice(test2[0:1])
 }
 
 func printSlice(x []int) {
