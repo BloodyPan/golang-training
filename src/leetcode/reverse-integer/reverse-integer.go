789ebff3fe3ff8293478bdd469e951f446e77d6c
Lu <vongolaluan@hotmail.com>
2020-12-28 17:50:49 +0800

    Update reverse-integer.go

--- src/leetcode/reverse-integer/reverse-integer.go
+++ src/leetcode/reverse-integer/reverse-integer.go
@@ -12,7 +12,7 @@ func reverse(x int) int {
 		radio = -1
 	}
 	for _, value := range strconv.Itoa(x * radio) {
-		fmt.Println(value)
+		//fmt.Println(value)
 		s = string(value) + s
 	}
 
@@ -24,6 +24,20 @@ func reverse(x int) int {
 	return 0
 }
 
+func reverse2(x int) int {
+	rev := 0
+	for x != 0 {
+		pop := x % 10
+		x = x / 10
+		if rev > 214748364 || rev < -214748364 {
+			return 0
+		}
+		rev = rev * 10 + pop
+	}
+	return rev
+}
+
 func main() {
 	fmt.Println(reverse(-120))
+	fmt.Println(reverse2(120))
 }
